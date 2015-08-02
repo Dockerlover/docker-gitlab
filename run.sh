c1431b7e235f6a3bafaaa6a4f9fe605bf8eabdfb
刘红 <liuhong1.happy@163.com>
2015-08-02 16:10:15 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it -d  --restart=always --name gitlab -v /var/data/gitlab:/var/opt/gitlab -v /var/log/gitlab:/var/log/gitlab -p 10080:80 -p 10022:22  docker-mongodb
+docker run -it -d  --restart=always --name gitlab -v /var/data/gitlab:/var/opt/gitlab -v /var/log/gitlab:/var/log/gitlab -p 10080:80 -p 10022:22  docker-gitlab
